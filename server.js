e5c4582ff1cd413cf96833050779d438a2b108b6
babislz <gabriela.laureano2007@gmail.com>
2024-09-25 16:58:29 -0300

    FINALLY IT WORKS

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.use(cors({
     origin: '*'
 }));
 
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb',extended: true}));
+
 require('./startup/routes')(app);
 
 const port = 8080;
